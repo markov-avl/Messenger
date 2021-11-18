10580de4b374ba3744b0b2bbef3ddeb4e636b9d3
markov-avl <andrewshasucks@gmail.com>
2021-11-18 22:49:24 +1000

    ШИЗААА

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 let chatSelector = document.getElementById('chatSelector')
 let messages = document.getElementById('messages')
+let textInput = document.getElementById('text')
+let send = document.getElementById('send')
 
 function scrollMessagesToBottom() {
     messages.scrollTop = messages.scrollHeight
 }
 
+function onSend() {
+    let message = textInput.value
+    if (message) {
+        document.getElementById('messages').innerHTML += `<div class="message" style="justify-content: right">
+                                                                       <div class="message__to">${message}</div>
+                                                                   </div>`
+        textInput.value = ''
+        scrollMessagesToBottom()
+        let reversedMessage = [...message].reverse().join("")
+        setTimeout(() => {
+            document.getElementById('messages').innerHTML += `<div class="message" style="justify-content: left">
+                                                                       <div class="message__from">${reversedMessage}</div>
+                                                                   </div>`
+            scrollMessagesToBottom()
+        }, 2000)
+
+    }
+}
+
 scrollMessagesToBottom()
+
+send.addEventListener('click', onSend)
